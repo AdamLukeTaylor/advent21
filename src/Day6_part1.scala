7f5261a78e7fdf3583f597ccbfab6a4970c0a796
Adam Taylor <tayloral@tcd.ie>
2021-12-08 18:41:39 +0000

    6 1

--- src/Day6_part1.scala
+++ src/Day6_part1.scala
@@ -1,49 +1,22 @@
 import scala.io.Source
 
-object Day5_part1 extends App {
-  val example = "data/day5_1_example"
-  val myData = "data/day5_1_data"
+object Day6_part1 extends App {
+  val example = "data/day6_1_example"
+  val myData = "data/day6_1_data"
   val dataFile = myData
-  val rawLines = Source.fromFile(dataFile).getLines.toList
+  var fish = Source.fromFile(dataFile).getLines.toList.head.split(",").map(_.toInt).toSeq
 
-  val lines = rawLines.map(Line.build)
-  println(s"${lines}")
-
-  var matrix = Seq.empty[Coord]
-  lines.foreach { line =>
-    val dx = math.signum(line.start.x - line.end.x)
-    val dy = math.signum(line.start.y - line.end.y)
-    def isDiagonal: Boolean = dx * dy != 0
-    def points: Seq[Coord] = if(!isDiagonal) {
-      Range.inclusive(0, math.max(math.abs(line.start.x - line.end.x), math.abs(line.start.y - line.end.y)))
-        .map(step => Coord(line.end.x + dx * step, line.end.y + dy * step))
-    } else Seq.empty[Coord]
-
-    matrix = matrix ++ points
+  for (a <- 1 to 81) {
+    println(s"Fish (day ${a - 1}): ${fish.size}")
+    fish = advanceFish(fish)
   }
 
-  println(s"${matrix.groupBy(identity).values.count(_.size > 1)}")
 
-  case class Line(start: Coord, end: Coord) {
-
+  def advanceFish(fish: Seq[Int]): Seq[Int] = {
+    val oldFish = fish.map(_ - 1)
+    val rebornFish = oldFish.count(_ < 0)
+    (oldFish.filter(_ >= 0) ++ Seq.fill(rebornFish)(6) ++ Seq.fill(rebornFish)(8))
   }
 
-  object Line {
-    def build(input: String): Line = {
-      val parts = input.split("->").toList
-      Line(start = Coord.build(parts.head), Coord.build(parts(1)))
-    }
-  }
-
-  case class Coord(x: Int, y: Int)
-
-  object Coord {
-    def build(input: String) = {
-      val parts = input.replaceAll("\\s", "").split(",")
-      Coord(x = parts.head.toInt, y = parts(1).toInt)
-    }
-  }
-
-  case class Count(coord: Coord, count: Int)
 
 }
