60395dacdd018e61a669af7dd69b26b667f9318a
Adam Taylor <tayloral@tcd.ie>
2021-12-21 17:35:51 +0000

    fuck this one

--- src/Day10_part1.scala
+++ src/Day10_part1.scala
@@ -1,40 +1,98 @@
+import scala.collection.mutable
 import scala.io.Source
 
 object Day10_part1 extends App {
   val example = "data/day10_1_example"
   val myData = "data/day10_1_data"
   val dataFile = example
-  val input = Source.fromFile(dataFile).getLines.toSeq//.filter(line => isNotValid(line.head, line.tail))
+  val input = Source.fromFile(dataFile).getLines.toSeq //.filter(line => isNotValid(line.head, line.tail))
   println(input.head)
-  println(removePair(input.head))
-  println(removePair(removePair(input.head)))
-  println(removePair(removePair(removePair(input.head))))
 
-  def isNotValid(input: String): Boolean = {
-    println(s"$input")
-    while (removePair(input).nonEmpty) {
+  input.foreach { lineIn =>
+    var line = lineIn
+    println(s"Running ${line}")
 
-
+    var open = mutable.Stack[(Char, Int)]()
+    open = open :+ (line.toList.zipWithIndex.head)
+    while (open.nonEmpty) {
+      val toTest: (Char, Int) = open.pop()
+      open.push(toTest)
+      try {
+        for (toCompare <- line.toList.zipWithIndex) {
+          if (toCompare._2 > toTest._2) {
+            println(s"Comparing ${toTest} and ${toCompare} ${line.toList(toTest._2)} ${line.toList(toCompare._2)}")
+            if (matchPair(line.toList(toTest._2), line.toList(toCompare._2))) {
+              line = crossOut(line, toTest, toCompare)
+            }
+            else {
+              if(line.toList(toTest._2) !='X') {
+                if (newCloser(toCompare._1)) throw NewCloser(toCompare)
+                if (newOpener(toCompare._1)) throw NewOpener(toCompare)
+              }
+              else{
+                if(open.nonEmpty) open.pop()
+                throw Matched(toTest)
+              }
+            }
+          }
+        }
+      } catch {
+        case NewOpener(a) => open.push(a)
+        case Matched(a) => {
+          println(s"Step ${line}")
+          line = crossOut(line, toTest, a)
+        }
+        case NewCloser(a) => {
+          println(s"Error ${a}")
+          open.clear()
+        }
+      }
     }
-    false
+    println(s"Finished ${containsClosed(line)} ${line}")
   }
 
-  def removePair(str: String): String = {
-    val target = str.head match {
+  def containsClosed(str: String): Boolean = {
+    str.contains(']') || str.contains(')') || str.contains('}') || str.contains('>')
+  }
+
+  def newOpener(test: Char): Boolean = {
+    test match {
+      case '(' => true
+      case '[' => true
+      case '{' => true
+      case '<' => true
+      case _ => false
+    }
+  }
+
+  def newCloser(test: Char): Boolean = {
+    test match {
+      case ')' => true
+      case ']' => true
+      case '}' => true
+      case '>' => true
+      case _ => false
+    }
+  }
+
+  def matchPair(source: Char, test: Char): Boolean = {
+    val target = source match {
       case '(' => ')'
       case '[' => ']'
       case '{' => '}'
       case '<' => '>'
+      case _ => 'X'
     }
-    val selection = str.toList.zipWithIndex.filter {
-      case (bracket, place) => bracket == str.head || bracket == target
-    }
-      val closer = selection.zipWithIndex.find{case ((bracket, innerPlace), place) => bracket == target}
-    if(closer.isEmpty) ""
-    else {
-      val closerLocation = closer.get._1._2
-      val openerLocation = selection(closer.get._2 - 1)._2
-      str.substring(0,openerLocation ) + str.substring(openerLocation + 1, closerLocation) + str.substring(closerLocation +1)
-    }
+    target == test
   }
+
+  def crossOut(input: String, open: (Char, Int), close: (Char, Int)): String = {
+    (input.substring(0, open._2) + "X" + input.substring(open._2 + 1)).substring(0, close._2) + "X" + input.substring(close._2 + 1)
+  }
+
+  case class Matched(toAdd: (Char, Int)) extends Throwable
+
+  case class NewOpener(toAdd: (Char, Int)) extends Throwable
+
+  case class NewCloser(toAdd: (Char, Int)) extends Throwable
 }
